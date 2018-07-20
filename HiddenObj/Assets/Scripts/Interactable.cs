0682c3a465335f424bd5fefeccb0b36c7e70eba2
Christine A. Brennan <christine@cabrennan.com>
2018-07-20 08:22:45 -0400

    Interactable
    Created interactible parent with radius of interaction

---
+++ HiddenObj/Assets/Scripts/Interactable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public float radius = 0.75f;
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with " + transform.name);
+    }
+}

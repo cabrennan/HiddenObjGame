f15024e16f77f0c854a4837fa2a8de664bfb5e93
Christine A. Brennan <christine@cabrennan.com>
2018-07-20 11:16:46 -0400

    Start Pickup code

---
+++ HiddenObj/Assets/Scripts/Item.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName ="Inventory/Item")]
+public class Item : ScriptableObject {
+
+    new public string name = "new item";
+    public string inventoryText = null;
+
+}
---
+++ HiddenObj/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class ItemPickup: Interactable {
+
+    public Item item;
+
+    public override void Interact()
+    {
+        base.Interact();
+        //Pickup();
+
+        
+    }
+
+    //void Pickup()
+    void OnMouseDown()
+    {
+        Debug.Log("picking up: " + item.name);
+        //remove from inventory
+        //remove from scene
+        Destroy(gameObject);
+    }
+}

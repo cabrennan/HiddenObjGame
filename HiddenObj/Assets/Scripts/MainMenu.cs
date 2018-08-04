8ff0d7d93533091cf1402a7535b86670a7828942
Christine A. Brennan <christine@cabrennan.com>
2018-08-04 09:22:55 -0400

    Main and Options Menu
    Implement simple Main & Options menu skeleton with simple play, quit, and back button functionality. Volume slider created but no script attached yet.

---
+++ HiddenObj/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void Play() 
+    {
+        Debug.Log("Load game map.");
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit(); 
+    }
+
+}
